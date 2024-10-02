c1f1acc858ddd7ae0fed2eda5e33409843d8a725
CarterChen247 <carterchen247@gmail.com>
2024-10-02 11:37:35 +0800

    feat: open developer settings page when quick settings tile is clicked

--- app/src/main/java/com/carterchen247/devshortcut/ShortcutTileService.kt
+++ app/src/main/java/com/carterchen247/devshortcut/ShortcutTileService.kt
@@ -1,12 +1,31 @@
 package com.carterchen247.devshortcut
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.provider.Settings
 import android.service.quicksettings.TileService
-import android.util.Log
+import android.widget.Toast
 
-class ShortcutTileService: TileService() {
+class ShortcutTileService : TileService() {
 
     override fun onClick() {
         super.onClick()
-        Log.d("ShortcutTileService", "onClick")
+        // check if development settings are enabled
+        val enabled = Settings.Secure.getInt(contentResolver, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0)
+        if (enabled == 0) {
+            val intent = Intent(Settings.ACTION_SETTINGS)
+            startActivityAndCollapseInternal(intent)
+            Toast.makeText(this, "Please enable development settings first", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val intent = Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
+        startActivityAndCollapseInternal(intent)
+    }
+
+    @SuppressLint("StartActivityAndCollapseDeprecated")
+    private fun startActivityAndCollapseInternal(intent: Intent) {
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivityAndCollapse(intent)
     }
 }
